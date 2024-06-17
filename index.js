c67183c7f71bf064fea22a46f9bfae058af48391
GoncaloNP <132508818+GoncaloNP@users.noreply.github.com>
2024-06-17 10:48:56 +0100

    Commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
- res.send('Olá Mundo bitches');
+ res.send('Olá Mundo');
 });
 
 app.listen(port, () => {
